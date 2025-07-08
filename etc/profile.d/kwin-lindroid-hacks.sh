58ef5c7d7bd2cad5f4cf3ef1954f9f6899fdc6c5
Luka Panio <lukapanio@gmail.com>
2025-07-08 15:17:11 +0200

    HACK Force dri card1 for now

--- etc/profile.d/kwin-lindroid-hacks.sh
+++ etc/profile.d/kwin-lindroid-hacks.sh
@@ -3,3 +3,5 @@ export KWIN_DRM_NO_AMS=1
 export KWIN_COMPOSE=O2ES
 export GBM_BACKEND=hybris
 export __GLX_VENDOR_LIBRARY_NAME=libhybris
+export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_libhybris.json
+export KWIN_DRM_DEVICES=/dev/dri/card1

e5092e6939f4f57e9a2d2addb29f30a74e3ce9f5
Mio <mio@mio19.uk>
2025-09-17 13:24:14 +1000

    set KWIN_DRM_DEVICES to the by-path one, which is consistent between multiple devices

--- etc/profile.d/kwin-lindroid-hacks.sh
+++ etc/profile.d/kwin-lindroid-hacks.sh
@@ -4,4 +4,4 @@ export KWIN_COMPOSE=O2ES
 export GBM_BACKEND=hybris
 export __GLX_VENDOR_LIBRARY_NAME=libhybris
 export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_libhybris.json
-export KWIN_DRM_DEVICES=/dev/dri/card1
+export KWIN_DRM_DEVICES=/dev/dri/by-path/platform-evdi-lindroid.0-card
